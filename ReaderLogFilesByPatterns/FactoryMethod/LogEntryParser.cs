2aa285017108f3b521dd0f2b456ee8931b414c6c
vmkovalkovskiy <vmkovalkovskiy@vodokanalekb.ru>
2021-05-01 20:56:39 +0500

    add parser.

--- ReaderLogFilesByPatterns/FactoryMethod/LogEntryParser.cs
+++ ReaderLogFilesByPatterns/FactoryMethod/LogEntryParser.cs
@@ -1,14 +1,36 @@
 namespace ReaderLogFilesByPatterns.FactoryMethod
 {
-    using Models;
+	using System.Collections.Generic;
+	using System.IO;
+	using Models;
 
-    public static class LogEntryParser
+    public class LogEntryParser
     {
+        private readonly TextReader _reader;
+
+        public LogEntryParser(TextReader reader)
+        {
+            _reader = reader;
+        }
+
         public static LogEntry Parse(string data)
         {
             // Анализирует содержание data и создает нужный
-            // экземпляр: ExceptionLogEntry или SimpleLogEntry
-            
+            // экземпляр: ExceptionLogEntry или SimpleLogEntry            
+            return new SimpleLogEntry();
+        }
+
+        public IEnumerable<LogEntry> Parse()
+        {
+            string line;
+            while ((line = _reader.ReadLine()) != null)
+            {
+                yield return ParseLine(line);
+            }
+        }
+
+        private LogEntry ParseLine(string line)
+        {
             return new SimpleLogEntry();
         }
     }
