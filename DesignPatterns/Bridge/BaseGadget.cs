66b1ac8915498ab5e0c1b146dc51c2b9f063da72
vmkovalkovskiy <vmkovalkovskiy@vodokanalekb.ru>
2021-05-13 01:33:48 +0500

    add bridge.

--- DesignPatterns/Bridge/BaseGadget.cs
+++ DesignPatterns/Bridge/BaseGadget.cs
@@ -17,15 +17,23 @@ namespace DesignPatterns.Bridge
 
         public void OnCharge()
         {
-            _energy = _adapter.GetEnergy();
+            _energy = _adapter?.GetEnergy();
         }
 
+        /// <summary>
+        /// Power, watt
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         protected int GetEnergy()
         {
+            if (string.IsNullOrEmpty(_energy))
+                throw new ArgumentNullException(nameof(_energy));
+            
             var array = _energy.Split(' ');
             var amperage = array.First(e => e.Contains("A"));
             amperage = amperage.Replace("A", "");
-            var voltage = array.Last(e => e.Contains("V"));
+            var voltage = array.First(e => e.Contains("V"));
             voltage = voltage.Replace("V", "");
             var power = int.Parse(amperage) * int.Parse(voltage);
             
